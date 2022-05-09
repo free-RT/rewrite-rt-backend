b3f40b7b0b7be98346fd2413cad30589d5a961b1
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-09 22:36:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from core import TypedSanic, setup_app
+from sanic import response
 from data import Secret
 
 
 app = TypedSanic("app")
+
+
+@app.route("/api")
+async def api(request):
+    return response.json({"hello": "world"})
 
 
 setup_app(app)
