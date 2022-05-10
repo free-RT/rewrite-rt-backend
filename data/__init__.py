912b9d44592e01ddaac6de99285644e6127333b5
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-10 11:39:21 +0900

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -5,6 +5,8 @@ from typing import TypedDict, Dict
 class SecretType(TypedDict):
     mysql: dict
     sanic: dict
+    hcaptcha: str
+    discord: dict
     
 with open("secret.json") as f:
     Secret: SecretType = loads(f.read())
